b95b92da22d138da02fa10aaf8403c4bd8bd3903
network2501 <pete@network2501.com>
2022-07-30 14:57:26 +0800

    DRY cleanup

--- 2021/day02/main.py
+++ 2021/day02/main.py
@@ -25,13 +25,14 @@ p1p = Position()
 
 def part1():
     for move in aoc_input:
+        tick = int(move[1])
         match move:
             case ["forward", str]:
-                p1p.horizontal_position += int(move[1])
+                p1p.horizontal_position += tick
             case ["down", str]:
-                p1p.depth += int(move[1])
+                p1p.depth += tick
             case ["up", str]:
-                p1p.depth -= int(move[1])
+                p1p.depth -= tick
 
 
 part1()
@@ -41,29 +42,24 @@ print(f"aim \t{p1p.aim}\ndepth \t{p1p.depth}\nHP \t{p1p.horizontal_position}")
 # Advent of Code 2021 Day 1 Part 2
 
 
-@dataclass
-class Position:
-    horizontal_position: int = 0
-    depth: int = 0
-    aim: int = 0
-
-
-p2p = Position
+p2p = Position()
 
 
 def part2():
     for move in aoc_input:
+        tick = int(move[1])
         match move:
             case ["forward", str]:
-                p2p.horizontal_position += int(move[1])
-                p2p.depth += Position.aim * int(move[1])
+                p2p.horizontal_position += tick
+                p2p.depth += tick * p2p.aim
             case ["down", str]:
-                p2p.aim += int(move[1])
+                p2p.aim += tick
             case ["up", str]:
-                p2p.aim -= int(move[1])
+                p2p.aim -= tick
 
 
 part2()
+
 
 print(f"\nDay 2 Part 1 Answer: \n{int(p2p.horizontal_position) * int(p2p.depth)}\n")
 print(f"aim \t{p2p.aim}\ndepth \t{p2p.depth}\nHP \t{p2p.horizontal_position}")
