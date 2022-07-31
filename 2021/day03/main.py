0f6a4293b0459146f4b41bb4b0b874f5a739b0ce
network2501 <pete@network2501.com>
2022-07-31 12:26:34 +0800

    cleaning up comments

--- 2021/day03/main.py
+++ 2021/day03/main.py
@@ -1,36 +1,6 @@
 # Advent of Code 2021 Day 3 Part 1
 # Answer 2640986
 
-
-#  0                   1                   2                   3
-#  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-# +-+-+-+-+-+
-# |0 0 1 0 0|
-# |1 1 1 1 0|
-# |1 0 1 1 0|
-# |1 0 1 1 1|
-# |1 0 1 0 1|
-# |0 1 1 1 1|
-# |0 0 1 1 1|
-# |1 1 1 0 0|
-# |1 0 0 0 0|
-# |1 1 0 0 1|
-# |0 0 0 1 0|
-# |0 1 0 1 0|
-# +-+-+-+-+-+
-#
-# Gamma rate is the most common number by column, convert from binary to decimal.
-#
-# Epsilon rate is the least common number by column (inverse gamma) convert
-# from binary to decimal.
-#
-# This is just begging for some ANDing
-# Or ZIP
-#
-# As it turned out zip() didn't really suit for the way I tackled this because
-# it was nested.
-
-
 from dataclasses import dataclass
 
 
@@ -49,21 +19,13 @@ aoc_input = aocinput()
 class Position:
     gamma: int = 0
     epsilon: int = 0
-    aim: int = 0
-
-
-# should get better with list comprehensions
-
-transformed = {}
+    oxygen_generator_rating: int = 0
+    CO2_scrubber_rating: int = 0
 
 
 def dict_maker():
-    # Assumes data is clean & creates a dict the size of the first value
-    # So if the string has 4 characters, this will make a dict with 4 keys
     positions = range(len(aoc_input[0]))
     d = dict(zip(positions, [None] * len(positions)))
-    # For the first value it'll need to replace the None value
-    # After that values should be appeneded.
     return d
 
 
@@ -72,13 +34,9 @@ def row_builder():
     for binary in aoc_input:
         for k, v in enumerate(binary):
             if d[k] is not None:
-                # Append a value to the string
                 d[k] += v
             else:
-                # replace the initialised None with a value
                 d[k] = v
-    # Don't want to deal with a dict type so return a list
-    # make me a list comp
     l = []
     for k, v in d.items():
         l.append(v)
@@ -88,7 +46,6 @@ def row_builder():
 def frequency():
     l = row_builder()
     counts = []
-    # this is binary so
     for i in l:
         one = 0
         zero = 0
